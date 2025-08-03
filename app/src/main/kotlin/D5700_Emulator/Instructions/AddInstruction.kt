3873dd925de4d3a7bf3b153ab960b54ac450354b
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-03 13:58:42 -0600

    got add instruction

--- app/src/main/kotlin/D5700_Emulator/Instructions/AddInstruction.kt
+++ app/src/main/kotlin/D5700_Emulator/Instructions/AddInstruction.kt
@@ -1,4 +1,17 @@
 package D5700_Emulator.Instructions
 
-class AddInstruction {
+import D5700_Emulator.CPU
+import D5700_Emulator.InputHandler
+import D5700_Emulator.Memory.RAM
+import D5700_Emulator.Memory.ROM
+import D5700_Emulator.Screen
+
+class AddInstruction(
+    private val rx: Int,
+    private val ry: Int,
+    private val rz: Int
+    ): Instruction(){
+    override fun execute(cpu: CPU, ram: RAM, rom: ROM, screen: Screen, input: InputHandler) {
+        cpu.registers[rz] = (cpu.registers[rx] + cpu.registers[ry]).toByte()
+    }
 }
--- app/src/main/kotlin/D5700_Emulator/Instructions/StoreInstruction.kt
+++ app/src/main/kotlin/D5700_Emulator/Instructions/StoreInstruction.kt
@@ -6,8 +6,11 @@ import D5700_Emulator.Memory.RAM
 import D5700_Emulator.Memory.ROM
 import D5700_Emulator.Screen
 
-class StoreInstruction(private val register: Int, private val value: Byte): Instruction() {
+class StoreInstruction(
+    private val rx: Int,
+    private val bb: Byte
+): Instruction() {
     override fun execute(cpu: CPU, ram: RAM, rom: ROM, screen: Screen, input: InputHandler) {
-        cpu.registers[register] = value
+        cpu.registers[rx] = bb
     }
 }
