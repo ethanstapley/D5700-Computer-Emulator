161392340a5c624049d3ef7488e153073033e2d1
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-03 15:53:53 -0600

    added ReadKeyboard Instruction

--- app/src/main/kotlin/D5700_Emulator/Instructions/ReadKeyboardInstruction.kt
+++ app/src/main/kotlin/D5700_Emulator/Instructions/ReadKeyboardInstruction.kt
@@ -1,4 +1,22 @@
 package D5700_Emulator.Instructions
 
-class ReadKeyboardInstruction {
+import D5700_Emulator.CPU
+import D5700_Emulator.InputHandler
+import D5700_Emulator.Memory.RAM
+import D5700_Emulator.Memory.ROM
+import D5700_Emulator.Screen
+
+class ReadKeyboardInstruction (
+    private val rx: Int
+): Instruction(){
+    override fun execute(cpu: CPU, ram: RAM, rom: ROM, screen: Screen, input: InputHandler) {
+        println("Input: ")
+        val input = readln().trim().uppercase().toCharArray()
+        val value = when {
+            input.isEmpty() -> 0
+            input.size > 2 -> throw IllegalStateException("Invalid Input, Char limit is 2")
+            else -> (String(input)).toInt(16)
+        }
+        cpu.registers[rx] = value.toByte()
+    }
 }
