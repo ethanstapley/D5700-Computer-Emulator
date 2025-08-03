84930aef910552baffc20bb07439de103b44bb18
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-03 16:37:29 -0600

    added convert to base ten instruction

---
+++ app/src/main/kotlin/D5700_Emulator/Instructions/ConvertToBaseTenInstruction.kt
@@ -0,0 +1,22 @@
+package D5700_Emulator.Instructions
+
+import D5700_Emulator.CPU
+import D5700_Emulator.InputHandler
+import D5700_Emulator.Memory.RAM
+import D5700_Emulator.Memory.ROM
+import D5700_Emulator.Screen
+
+class ConvertToBaseTenInstruction (
+    private val rx: Int
+): Instruction(){
+    override fun execute(cpu: CPU, ram: RAM, rom: ROM, screen: Screen, input: InputHandler) {
+        val value = cpu.registers[rx].toInt() and 0xFF
+        val hundreds = value / 100
+        val tens = (value % 100) / 10
+        val ones = value % 10
+
+        ram.write(cpu.A, hundreds.toByte())
+        ram.write(cpu.A + 1, tens.toByte())
+        ram.write(cpu.A + 2, ones.toByte())
+    }
+}
