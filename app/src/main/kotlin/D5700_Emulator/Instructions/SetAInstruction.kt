8efe4b8f065f63511639a5abf3a444a75515a7b7
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-03 16:15:43 -0600

    added set T and A instructions

--- app/src/main/kotlin/D5700_Emulator/Instructions/SetAInstruction.kt
+++ app/src/main/kotlin/D5700_Emulator/Instructions/SetAInstruction.kt
@@ -1,4 +1,13 @@
 package D5700_Emulator.Instructions
 
-class SetAInstruction {
+import D5700_Emulator.CPU
+import D5700_Emulator.InputHandler
+import D5700_Emulator.Memory.RAM
+import D5700_Emulator.Memory.ROM
+import D5700_Emulator.Screen
+
+class SetAInstruction(private val aaa: Int) : Instruction() {
+    override fun execute(cpu: CPU, ram: RAM, rom: ROM, screen: Screen, input: InputHandler) {
+        cpu.A = aaa
+    }
 }
