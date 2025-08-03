94d75ed446520d17f8e9dc57cadf02f0d4ce7003
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-03 17:34:23 -0600

    added convert To ascii

--- app/src/main/kotlin/D5700_Emulator/Instructions/ConvertByteToAsciiInstruction.kt
+++ app/src/main/kotlin/D5700_Emulator/Instructions/ConvertByteToAsciiInstruction.kt
@@ -1,4 +1,21 @@
 package D5700_Emulator.Instructions
 
-class ConvertByteToAsciiInstruction {
+import D5700_Emulator.CPU
+import D5700_Emulator.InputHandler
+import D5700_Emulator.Memory.RAM
+import D5700_Emulator.Memory.ROM
+import D5700_Emulator.Screen
+
+class ConvertByteToAsciiInstruction(
+    private val rx: Int,
+    private val ry: Int
+): Instruction(){
+    override fun execute(cpu: CPU, ram: RAM, rom: ROM, screen: Screen, input: InputHandler) {
+        val value = cpu.registers[rx].toInt() and 0xFF
+        if (value > 0xF) throw IllegalArgumentException("Value in register must be between 0 and F to convert to ASCII")
+
+        val ascii = if (value < 10) (value + '0'.code) else (value - 10 + 'A'.code)
+
+        cpu.registers[ry] = ascii.toByte()
+    }
 }
