78ef3f93ed2ff5e156ecd8c06c94231793d3f4db
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-03 14:02:53 -0600

    added subtraction

--- app/src/main/kotlin/D5700_Emulator/Instructions/SubInstruction.kt
+++ app/src/main/kotlin/D5700_Emulator/Instructions/SubInstruction.kt
@@ -1,4 +1,17 @@
 package D5700_Emulator.Instructions
 
-class SubInstruction {
+import D5700_Emulator.CPU
+import D5700_Emulator.InputHandler
+import D5700_Emulator.Memory.RAM
+import D5700_Emulator.Memory.ROM
+import D5700_Emulator.Screen
+
+class SubInstruction(
+    private val rx: Int,
+    private val ry: Int,
+    private val rz: Int
+): Instruction(){
+    override fun execute(cpu: CPU, ram: RAM, rom: ROM, screen: Screen, input: InputHandler) {
+        cpu.registers[rz] = (cpu.registers[ry] - cpu.registers[rx]).toByte()
+    }
 }
