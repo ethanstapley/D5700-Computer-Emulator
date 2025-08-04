1f8f493547f4c154b6809596dc376af3e88a56fe
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-03 19:50:24 -0600

    added the cpu

--- app/src/main/kotlin/D5700_Emulator/CPU.kt
+++ app/src/main/kotlin/D5700_Emulator/CPU.kt
@@ -1,6 +1,8 @@
 package D5700_Emulator
 
 import D5700_Emulator.Instructions.Instruction
+import D5700_Emulator.Memory.RAM
+import D5700_Emulator.Memory.ROM
 
 class CPU {
     val registers = ByteArray(8)
@@ -10,16 +12,24 @@ class CPU {
     var M: Boolean = false
     var halted: Boolean = false
 
-    fun tick() {
+    fun tick(ram: RAM, rom: ROM, screen: Screen, factory: InstructionFactory) {
+        if (halted) return
 
+        val instruction = loadInstruction(ram, rom, factory)
+        instruction.execute(this, ram, rom, screen)
+
+        if (instruction.autoIncrementPC) {
+            P += 2
+        }
     }
 
-    fun executeInstruction(opcode: Byte, operands: ByteArray) {
 
+    fun loadInstruction(ram: RAM, rom: ROM, factory: InstructionFactory): Instruction {
+        val memory = if (M) rom else ram
+        val b1 = memory.read(P)
+        val b2 = memory.read(P + 1)
+        return factory.getInstruction(b1, b2)
     }
 
-//    fun loadInstruction(): Instruction {
-//        return
-//    }
 
 }
--- app/src/main/kotlin/D5700_Emulator/D5700Emulator.kt
+++ app/src/main/kotlin/D5700_Emulator/D5700Emulator.kt
@@ -1,5 +1,21 @@
 package D5700_Emulator
 
+import D5700_Emulator.Memory.*
+import D5700_Emulator.Timer
+
 class D5700Emulator {
+    private val cpu = CPU()
+    private val ram = RAM()
+    private val rom = ROM()
+    private val screen = Screen()
+    private val timer = Timer
+
+    fun start(programPath: String) {
+
+    }
+
+    fun stop() {
+
+    }
 
 }
